c77f06f08b5efc5cc45cfbb2dd3721a0ddd0dc17
José Solorzano <solorzano202009@gmail.com>
2018-06-22 11:21:23 -0400

    Añadidas las funcionalidades para publicación y eliminación de posts.

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,11 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.post_list, name='home'),
+	url(r'^$', views.post_list, name='post_list'),
+	url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
 	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 	url(r'^post/edit/(?P<pk>[0-9]+)/$', views.post_edit, name='post_edit'),
+	url(r'^post/publish/(?P<pk>[0-9]+)/$', views.post_publish, name='post_publish'),
+	url(r'^post/remove/(?P<pk>[0-9]+)/$', views.post_remove, name='post_remove'),
 	url(r'^post/new/$', views.post_new, name='post_new')
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,13 +8,22 @@ from .forms import PostForm
 def post_list(request):
 	
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	
 	return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_draft_list(request):
+	
+	posts = Post.objects.filter(published_date__isnull=True).order_by('published_date')
+	
+	return render(request, 'blog/post_draft_list.html', {'posts': posts})
 	
 	
 def post_detail(request, pk):
 	
 	#~ post = Post.objects.get(pk=pk)
 	post = get_object_or_404(Post, pk=pk)  # Devuelve un mensaje de error si recibe un id no existente
+	
 	return render(request, 'blog/post_detail.html', {'post': post})
 	
 	
@@ -27,7 +36,7 @@ def post_new(request):
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
-			post.published_date = timezone.now()
+			#~ post.published_date = timezone.now()
 			post.save()
 			return redirect('post_detail', pk=post.pk)
 	else:
@@ -50,3 +59,19 @@ def post_edit(request, pk):
 		form = PostForm(instance=post)
 	
 	return render(request, 'blog/post_edit.html', {'form': form})
+	
+	
+def post_publish(request, pk):
+	
+	post = get_object_or_404(Post, pk=pk)
+	post.publish()
+	
+	return redirect('post_detail', pk=pk)
+	
+	
+def post_remove(request, pk):
+	
+	post = get_object_or_404(Post, pk=pk)
+	post.delete()
+
+	return redirect('post_list')
